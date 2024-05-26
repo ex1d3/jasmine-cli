0b1696942c31641bcf7089cffab5a6ccbd93d014
ex1d3 <okolobaxha@yandex.ru>
2024-05-26 22:11:42 +0700

    feat(storage): add methods for clear Src and Tx storages

--- storage/src.go
+++ storage/src.go
@@ -1,3 +1,9 @@
 package storage
 
 var Src = make(map[string]bool)
+
+func ClearSrc() {
+	for k := range Src {
+		delete(Src, k)
+	}
+}
--- storage/tx.go
+++ storage/tx.go
@@ -3,3 +3,9 @@ package storage
 import "nolono-cli/domain"
 
 var Tx = make(map[string]*domain.Tx)
+
+func ClearTx() {
+	for k := range Tx {
+		delete(Tx, k)
+	}
+}
