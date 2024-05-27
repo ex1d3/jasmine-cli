bef9e8a7f496b10efba7dd067c69decb43116d23
ex1d3 <okolobaxha@yandex.ru>
2024-05-27 11:12:03 +0700

    feat(exector/commands/internal_errors): add "invalid_args_count" error

--- executor/commands/internal_errors/invalid_args_count.go
+++ executor/commands/internal_errors/invalid_args_count.go
@@ -2,9 +2,14 @@ package internal_errors
 
 import "fmt"
 
-func InvalidArgsCount(cmd string, expected int, received int) string {
+func InvalidArgsCount(
+	cmd string,
+	// for support "from x to y" statements in errors
+	expected string,
+	received int,
+) string {
 	return fmt.Sprintf(
-		"invalid args count for '%s' command (expected: %d, received: %d)",
+		"invalid args count for '%s' command (expected: %s, received: %d)",
 		cmd,
 		expected,
 		received,
