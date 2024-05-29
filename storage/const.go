619ae92f0936e9b171ababd28394404867933fd8
ex1d3 <okolobaxha@yandex.ru>
2024-05-29 16:56:09 +0700

    chore(executor/commands): unify "get" command exeuctor function

--- storage/const.go
+++ storage/const.go
@@ -2,5 +2,5 @@ package storage
 
 import "jasmine-cli/domain"
 
-var Src = NewJSONStorage[string, bool]("src-storage.json")
+var Src = NewJSONStorage[string, *domain.Src]("src-storage.json")
 var Tx = NewJSONStorage[string, *domain.Tx]("tx-storage.json")
