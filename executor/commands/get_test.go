bfe9cdca47390c8d65a6dd85331ddc77199c1332
артур <okolobaxha@yandex.ru>
2024-06-05 13:17:04 +0700

    test(executor/commands): make test fall message for "get" conventional

--- executor/commands/get_test.go
+++ executor/commands/get_test.go
@@ -1,6 +1,7 @@
 package commands
 
 import (
+	"fmt"
 	"jasmine-cli/domain"
 	"jasmine-cli/storage"
 	"strings"
@@ -13,62 +14,94 @@ func TestGetAllTxs(t *testing.T) {
 
 	storage.Src.Set("adam", domain.NewSrc("adam"))
 
-	firstTx := domain.NewTx("1", "adam", 200)
-	secondTx := domain.NewTx("2", "adam", 30)
+	want := [2]*domain.Tx{
+		domain.NewTx("1", "adam", 200),
+		domain.NewTx("2", "adam", 30),
+	}
 
-	txStorage.Set("1", firstTx)
-	txStorage.Set("2", secondTx)
+	txStorage.Set("1", want[0])
+	txStorage.Set("2", want[1])
 
-	txs, err := Get(strings.Split("tx *", " "))
+	command := strings.Split("tx *", " ")
+	txs, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		txs,
+		err,
+		want,
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatalf(errorMessage)
 	}
 
 	if len(txs) != 2 {
-		t.Fatal("unexpected return value length")
+		t.Fatalf(errorMessage)
 	}
 
 	for _, tx := range txs {
-		if tx != firstTx && tx != secondTx {
-			t.Fatal("unexpected return value")
+		if tx != want[0] && tx != want[1] {
+			t.Fatalf(errorMessage)
 		}
 	}
 }
 
-func TestGetExistingTx(t *testing.T) {
-	txId := "3"
-	tx := domain.NewTx("1", "ben", 20)
+func TestGetTx(t *testing.T) {
+	txId := "1"
+	tx := domain.NewTx(txId, "ben", 20)
 
 	storage.Src.Set("ben", domain.NewSrc("ben"))
 	storage.Tx.Set(txId, tx)
 
-	txs, err := Get(strings.Split("tx 3", " "))
+	command := strings.Split(fmt.Sprintf("tx %s", txId), " ")
+	txs, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		txs,
+		err,
+		[]interface{}{tx},
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatalf(errorMessage)
 	}
 
 	if len(txs) != 1 {
-		t.Fatal("unexpected return value length")
+		t.Fatalf(errorMessage)
 	}
 
-	if txs[0] != storage.Tx.Get(txId) {
-		t.Fatal("unexpected return value")
+	if txs[0] != tx {
+		t.Fatalf(errorMessage)
 	}
 }
 
-func TestGetNonExistingTx(t *testing.T) {
+func TestGetInvalidTx(t *testing.T) {
 	storage.Tx.Unload()
 
-	txs, err := Get(strings.Split("tx 1", " "))
+	command := strings.Split("tx 1", " ")
+	txs, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		txs,
+		err,
+		[]interface{}{},
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatal(errorMessage)
 	}
 
 	if len(txs) != 0 {
-		t.Fatal("unexpected return value length")
+		t.Fatal(errorMessage)
 	}
 }
 
@@ -77,86 +110,141 @@ func TestGetAllSrcs(t *testing.T) {
 
 	srcStorage.Unload()
 
-	adam := domain.NewSrc("adam")
-	ben := domain.NewSrc("ben")
+	want := [2]*domain.Src{
+		domain.NewSrc("adam"),
+		domain.NewSrc("ben"),
+	}
+
+	adam := want[0]
+	ben := want[1]
 
 	srcStorage.Set("adam", adam)
 	srcStorage.Set("ben", ben)
 
-	srcs, err := Get(strings.Split("src *", " "))
+	command := strings.Split("src *", " ")
+	srcs, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		srcs,
+		err,
+		want,
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatal(errorMessage)
 	}
 
 	if len(srcs) != 2 {
-		t.Fatal("unexpected return value length")
+		t.Fatal(errorMessage)
 	}
 
 	for _, src := range srcs {
 		if src != adam && src != ben {
-			t.Fatal("unexpected return value")
+			t.Fatal(errorMessage)
 		}
 	}
 }
 
-func TestGetExistingSrc(t *testing.T) {
+func TestGetSrc(t *testing.T) {
 	srcStorage := storage.Src
 
 	srcStorage.Unload()
 
 	adam := domain.NewSrc("adam")
-
 	srcStorage.Set("adam", adam)
-	srcs, err := Get(strings.Split("src adam", " "))
+
+	command := strings.Split("src adam", " ")
+	srcs, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		srcs,
+		err,
+		[]interface{}{adam},
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatal(errorMessage)
 	}
 
 	if len(srcs) != 1 {
-		t.Fatal("unexpected return value length")
+		t.Fatal(errorMessage)
 	}
 
 	if srcs[0] != adam {
-		t.Fatal("unexpected return value")
+		t.Fatal(errorMessage)
 	}
 }
 
-func TestGetNonExistingSrc(t *testing.T) {
+func TestGetInvalidSrc(t *testing.T) {
 	storage.Src.Unload()
 
-	src, err := Get(strings.Split("src adam", " "))
+	command := strings.Split("src adam", " ")
+	src, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		src,
+		err,
+		[]interface{}{},
+		nil,
+	)
 
 	if err != nil {
-		t.Fatal("unexpected error")
+		t.Fatal(errorMessage)
 	}
 
 	if len(src) != 0 {
-		t.Fatal("unexpected return value length")
+		t.Fatal(errorMessage)
 	}
 }
 
-func TestGetWithUnexpectedArgsCount(t *testing.T) {
-	result, err := Get(strings.Split("abc abc abc", " "))
+func TestGetWithUnexpectedArgs(t *testing.T) {
+	command := strings.Split("abc abc abc", " ")
+	result, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		result,
+		err,
+		[]interface{}{},
+		"error",
+	)
 
 	if err == nil {
-		t.Fatal("call with unexpected args count not pervented")
+		t.Fatal(errorMessage)
 	}
 
 	if len(result) != 0 {
-		t.Fatal("unexpected return value len")
+		t.Fatal(errorMessage)
 	}
 }
 
-func TestGetFromNonexistingCollection(t *testing.T) {
-	result, err := Get(strings.Split("abc abc", " "))
+func TestGetWithInvalidCollection(t *testing.T) {
+	command := strings.Split("abc abc", " ")
+	result, err := Get(command)
+
+	errorMessage := fmt.Sprintf(
+		"Get(%q) = %v, %v; want %v, %v",
+		command,
+		result,
+		err,
+		[]interface{}{},
+		"error",
+	)
 
 	if err == nil {
-		t.Fatal("call to non existing collection not pervented")
+		t.Fatal(errorMessage)
 	}
 
 	if len(result) != 0 {
-		t.Fatal("unexpected return value len")
+		t.Fatal(errorMessage)
 	}
 }
